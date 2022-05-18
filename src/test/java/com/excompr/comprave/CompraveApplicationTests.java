76e2d4e06556879c587131942fc83ad3eb7a9826
Francesco <francesco.serra8280@outlook.it>
2022-05-18 16:40:47 +0200

    progetto aggiunto

---
+++ src/test/java/com/excompr/comprave/CompraveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excompr.comprave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
